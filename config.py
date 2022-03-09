6fe8ba927d5676298dc8efc7f4cf3a60fc512bd2
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2022-03-09 12:08:15 +0100

    Ignorecase fixes

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ def pprint(msg: str):
     print(f"{msg}\n{PROMPT}")
 
 def handle_subprocess(cmd: list[str]) -> Optional[str]:
-    process_result: CompletedProcess = subprocess.run(cmd, capture_output=True)
+    process_result: subprocess.CompletedProcess = subprocess.run(cmd, capture_output=True)
     if process_result.returncode != 0:
         raise Exception(f"An error occured: {cmd.stderr}")
     else:
@@ -86,7 +86,9 @@ while True:
         continue
 
     vbox_ip_stdout = handle_subprocess(os_shell + [f"VBoxManage guestproperty enumerate {vm_name} | findstr /i ip"])
-    if (main_ip_match := re.match(r".*?/Net/0/.*?value:\s+(?P<IP>\d+\.\d+\.\d+\.\d+)", vbox_ip_stdout)) and main_ip_match.group("IP"):
+    if ((main_ip_match := re.match(r".*?/Net/0/.*?value:\s+(?P<IP>\d+\.\d+\.\d+\.\d+)", vbox_ip_stdout, flags=re.IGNORECASE)) 
+        and main_ip_match.group("IP")
+        ):
         vm_ip: str = main_ip_match.group("IP")
         if not vm_ip:
             print(f"VM IP not found!")
@@ -99,14 +101,29 @@ while True:
         with ssh_config_path.open(mode="r+") as ssh_config_file:
             ssh_config: str = ssh_config_file.read()
             
-            vm_config_pattern = re.compile(rf".*?(?P<VMConfig>^Host {vm_name}.*?^(?!\s+))", re.DOTALL | re.IGNORECASE | re.MULTILINE)
+            vm_config_pattern = re.compile(rf".*?(?P<VMConfig>^host {vm_name}.*?^(?!\s+))", re.DOTALL | re.IGNORECASE | re.MULTILINE)
             if ((vm_config_slice_match := vm_config_pattern.match(ssh_config))
                 and vm_config_slice_match.group("VMConfig")
                 ):
                 
                 vm_config_slice: str = vm_config_slice_match.group("VMConfig")
-                vm_config_slice_updated: str = re.sub(r"Hostname\s+\S+", f"Hostname {vm_ip}", vm_config_slice)
-                # ssh_config_update_test: str = re.sub(vm_config_pattern, vm_config_slice_updated, ssh_config)
+
+                hostname_pattern: re.Pattern = re.compile(r"hostname\s+(\d+\.\d+\.\d+\.\d+)", flags=re.IGNORECASE)
+                current_vm_ip: str = ""
+                if ((current_vm_ip_match := hostname_pattern.search(vm_config_slice))
+                    and current_vm_ip_match.group(1)
+                    ):
+                    current_vm_ip = current_vm_ip_match.group(1)
+
+                if current_vm_ip and vm_ip and current_vm_ip == vm_ip:
+                    print(f"The {vm_name}\'s config seems to be up-to-date with ip: {current_vm_ip}")
+                    break
+
+                vm_config_slice_updated: str = hostname_pattern.sub(f"HostName {vm_ip}", vm_config_slice)
+
+                if vm_config_slice == vm_config_slice_updated:
+                    raise("Error. No update performed.")
+
                 ssh_config_updated = ssh_config.replace(vm_config_slice, vm_config_slice_updated)
             else:
                 print(f"No config found for vm: {vm_name}")
@@ -116,7 +133,7 @@ while True:
         with ssh_config_path.open(mode="w") as ssh_config_file:
             if ssh_config_updated:
                 ssh_config_file.write(ssh_config_updated)
-                print(f"Update successful.")
+                print(f"Update successful. {vm_name}\' ip is now: {vm_ip}")
                 break
             else:
                 print("Update unsuccessful.")
